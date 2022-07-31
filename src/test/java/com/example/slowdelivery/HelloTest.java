581fbf554b2b7eeba2c159b832ff461980850f53
= <xman96@naver.com>
2022-07-31 21:55:25 +0900

    FEAT : Jacoco 연동 완료. 테스트커버리지 설정 완료

---
+++ src/test/java/com/example/slowdelivery/HelloTest.java
@@ -0,0 +1,13 @@
+package com.example.slowdelivery;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class HelloTest {
+
+    @Test
+    void 테스트용_테스트() {
+
+        Assertions.assertThat(1).isEqualTo(1);
+    }
+}
